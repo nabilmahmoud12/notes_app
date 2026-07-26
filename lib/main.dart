815e25da54c76065c0acff2117aad7fc4b2e36f6
nabilmahmoud12 <nabilmeywis@gmail.com>
2026-07-26 12:56:34 +0300

    create bloc observer and add bloc observer to main

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:notsapp/Views/notes_view.dart';
 import 'package:notsapp/cubits/cubit/add_note_cubit.dart';
 import 'package:notsapp/models/note_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notsapp/simple_bloc_observer.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter(); //from flutter
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox(kNotesBox);
